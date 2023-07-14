3c71dd4a95123d9551dfee5ef8701b499b07e155
Daniel Pineda <angeldpb27@gmail.com>
2023-07-13 20:18:23 -0500

    [+] app-routing.module: Registra rutas /, /login, /register, **

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+// http://localhost:4200
+const routes: Routes = [                                 // -> path   page
+  {path: '', component: HomeComponent},                  // -> /           home page       http://localhost:4200/
+  {path: 'login', component: LoginComponent},            // -> /login      login page      http://localhost:4200/login
+  {path: 'register', component: RegisterComponent},      // -> /register   register page   http://localhost:4200/register
+  {path: '**', redirectTo: ''}                           // -> /hola       home page       http://localhost:4200/   esta ruta redirige a home en caso que la ruta especificada no existe
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
