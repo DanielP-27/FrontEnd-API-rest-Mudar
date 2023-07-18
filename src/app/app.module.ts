ba4ec27f1e9644c5f342d151bf0c48d85d870409
Daniel Pineda <angeldpb27@gmail.com>
2023-07-17 20:08:05 -0500

    [+] add.module: importar ReactiveFormModules [+] implementacion y validacion formularios reactivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
@@ -19,7 +21,8 @@ import { FooterComponent } from './shared/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  // atributos de la clase
+  // esta clase debe estar incluida en el archivo HTML; en este caso [formGroup]="loginForm"
+  loginForm: FormGroup = this.fb.group({
+    // '' valor predeterminado del campo; por lo general se deja vacio 
+    // [] corresponde a la validaciones => Validators
+    email: [ '', [Validators.required, Validators.email] ], 
+    password: ['', [Validators.required, Validators.minLength( 8 )]]
+  });
 
+  // inyección de dependencias utilizando constructor
+  constructor ( private fb: FormBuilder ) {};
+  
+  // metodo para el login
+  login() {
+    console.group( 'loginForm' );
+    console.info(this.loginForm.value);
+    console.log(this.loginForm.valid);
+    console.group();
+  }
 }
