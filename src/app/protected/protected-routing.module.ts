d22f2a997a4855037faedb1717cc7af4e65bc88a
Daniel Pineda <angeldpb27@gmail.com>
2023-07-17 21:21:11 -0500

    [+] ng g m protected --routing [+] ng g protected/pages/dashboard [+] app-routing.module: definir ruta de modulo 'dashboard' [+] protected-routing.module: definir rutas hijas del modulo 'dashboard'

---
+++ src/app/protected/protected-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+const routes: Routes = [
+  {
+    // http://localhost:4200/dashboard
+    path: '', 
+    children: [
+      {
+        path: '',
+        component: DashboardComponent
+      },
+      {
+        path: '**',
+        redirectTo: ''
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProtectedRoutingModule { }
---
+++ src/app/protected/protected.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProtectedRoutingModule } from './protected-routing.module';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    ProtectedRoutingModule
+  ]
+})
+export class ProtectedModule { }
