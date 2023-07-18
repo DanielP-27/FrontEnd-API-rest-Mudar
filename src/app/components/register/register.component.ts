250203fd1c53e56441931be6cd1d2529fd30ded4
Daniel Pineda <angeldpb27@gmail.com>
2023-07-17 20:38:40 -0500

    [+] register.component: se implementan formularios reactivos

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+  // atributos
+  registerForm: FormGroup = this.fb.group ({
+    // '' valor por defecto de los campos
+    // [] validaciones para los campos
+    nombre: [ '', [Validators.required]],
+    email: [ '', [Validators.required, Validators.email]],
+    password: [ '', [Validators.required, Validators.minLength( 8 )]]
+  })
+  // constructor: inyeccion de dependencias
+  constructor( private fb: FormBuilder ) {}
+  // Metodo: Register
+  register() {
+    console.group('registerForm');
+    console.info( this.registerForm.value);
+    console.info(this.registerForm.valid);
+    console.group();
+  }
 }
